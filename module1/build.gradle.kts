dd85e15e4931ade778038ee0e04494b1094b8978
power <omarsief425@gmail.com>
2025-04-10 19:18:39 +0200

    d

--- module1/build.gradle.kts
+++ module1/build.gradle.kts
@@ -1,6 +1,24 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm")
+}
+
 group = rootProject.group
 version = rootProject.version
 
+repositories {
+    mavenCentral()
+}
+
 dependencies {
-    project(":module2")
+    implementation(project(":module2"))
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.0")
+    runtimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+    runtimeOnly("org.junit.platform:junit-platform-console:1.9.0")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
